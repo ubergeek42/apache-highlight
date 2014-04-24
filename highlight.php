432b2f618ed4f6df62a962fe95451e756081a0d4
Keith Johnson <kj@ubergeek42.com>
2014-04-24 16:39:36 -0400

    Make more configurable
    Add option for line wrapping/showing invisible characters. These were
    also added to the UI on the page
    Option in the php to use the full browser width for the editor
    Custom CSS variable for any tweaks someone might want to make

--- highlight.php
+++ highlight.php
@@ -1,7 +1,10 @@
 <?php
 // Options
-$theme = 'tomorrow'; // What ace theme to use
-$fontsize = '12px';  // Default font size to display
+$theme = 'Eclipse';         // What ace theme to use('Eclipse', 'tomorrow' are good choices)
+$fontsize = '12pt';         // Default font size to display
+$wrap_lines = 'true';       // Wrap long lines?  (*MUST* be a string, 'true' or 'false')
+$show_invisibles = 'false'; // show whitespace   (*MUST* be a string, 'true' or 'false')
+$full_width = false;        // Use the full browser width?
 
 /**
 Your .htaccess file should look something like this(You will need mod_actions enabled)
@@ -20,6 +23,19 @@ Your .htaccess file should look something like this(You will need mod_actions en
     AddHandler highlight-code .java
 */
 
+// Any custom css you want applied goes here:
+$custom_css = "
+    /* style applied to line numbers in the gutter */
+    .ace_gutter-cell {
+        /* Custom color for the line number color */
+        color: #999;
+
+        /* If you change this font size for line numbers, you will need to set the line-height
+           to the height of the row. */
+        /* font-size: 10pt;    */
+        /* line-height: 22px;  */ /* 22px is the height when using 14pt $fontsize */
+    }
+";
 
 //====================================================================================
 // No need to edit below this point
@@ -55,9 +71,15 @@ if ($filecontents === FALSE) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <style type="text/css" media="screen">
+        html, body {
+            height: 100%;
+        }
         .container {
-            position:relative;
-            height:100%;
+            position: relative;
+            height: 100%;
+            <?php if ($full_width): ?>
+            width:100%;
+            <?php endif;?>
         }
         #editor { 
             position: relative !important;
@@ -71,46 +93,67 @@ if ($filecontents === FALSE) {
             margin-bottom: 10px;
             margin-top: 10px;
         }
-        #header div {
-			float:left;
-			height: 50px;
+        #header .title h1 {
+            float:left;
+            height: 50px;
+            margin: 0px;
+            margin-right: 20px;
         }
-		#header .title h1 {
-			margin: 0px;
-		}
-		#header .info {
-			margin-left: 20px;
-		}
-        #footer {
+        #header .info {
+            float:left;
+            height: 50px;
+        }
+        #header .toolbox {
+            height: 50px;
+            float: right;
+        }
+        .toolbox .checkbox {
+            margin: 0px;
+            padding: 0px;
+        }
+        #footer p{
             margin-top: 25px;
             margin-bottom: 15px;
             text-align: center;
-            font-size: 12px;
+            font-size: 12pt;
         }
 
-        .ace_gutter-cell {
-            color: #ccc;
-        }
+        <?=$custom_css;?>
     </style>
 </head>
 <body>
 <div class="container">
-    <div id="header" class="row">
+    <div id="header">
 		<div class="title"><h1><?=basename($filename)?></h1></div>
 		<div class="info">
         <a href="?raw">Download/View Raw File</a><br>
 		Filetype: <span id="filetype">Unknown</span>
 		</div>
+        <div class="toolbox">
+            <div class="checkbox">
+                <label for="show_invisibles">Show hidden characters
+                <input type="checkbox"
+                       onchange="editor.setShowInvisibles(this.checked);"
+                       id="show_invisibles" <?php echo ($show_invisibles=='true'?'CHECKED':'')?>></input>
+                </label>
+                <br>
+                <label for="wrap_lines">Wrap long lines
+                <input type="checkbox"
+                       onchange="editor.getSession().setUseWrapMode(this.checked);"
+                       id="wrap_lines" <?php echo ($wrap_lines=='true'?'CHECKED':'')?>></input>
+                </label>
+            </div>
+        </div>
     </div>
-    <div class="row">
+    <div class="row content">
 	<?php if (!$error): ?>
         <pre id="editor"><?php echo htmlspecialchars($filecontents); ?></pre>
 	<?php else: ?>
 		<pre class="error"><?=$error?></pre>
 	<?php endif ?>
     </div>
-    <div class="row">
-    <p id="footer">
+    <div id="footer">
+    <p>
         Syntax highlighting provided by <a href="http://ace.c9.io">Ace</a>.
     </p>
     </div>
@@ -120,16 +163,16 @@ if ($filecontents === FALSE) {
 <script src="//cdnjs.cloudflare.com/ajax/libs/ace/1.1.3/ext-modelist.js" type="text/javascript" charset="utf-8"></script>
 <?php if (!$error): ?>
 <script>
-    var editor = ace.edit("editor");
+    editor = ace.edit("editor");
     editor.setTheme("ace/theme/<?=$theme?>");
 
-    editor.getSession().setUseWrapMode(true);
+    editor.getSession().setUseWrapMode(document.getElementById("wrap_lines").checked);
     editor.setReadOnly(true);
 
     // Make the editor as big as the file to prevent scrolling(or 50 lines min)
-    editor.setAutoScrollEditorIntoView();
     editor.setOption("maxLines", Infinity);
     editor.setOption("minLines", 50);
+    editor.setShowInvisibles(document.getElementById("show_invisibles").checked);
 
     // Autodetect mode based on file name
     var modelist = ace.require('ace/ext/modelist');
